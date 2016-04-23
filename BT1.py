e6961c6bd0c66153f5bd6e2e227f985a68d4246e
thanhsonlpc97 <thanhsonlpc97@users.noreply.github.com>
2016-04-23 17:34:14 +0700

    Create BT1.py

---
+++ BT1.py
@@ -0,0 +1,10 @@
+score = int(input())
+if score >= 90:
+    print('Grade is A')
+elif score >= 80:
+    print('Grade is B')
+elif score >= 70:
+    print('Grade is C')
+elif score >= 60:
+    print('Grade is D')
+else: print('Grade is F')
